4ae06d490263eb4bd2f3ab738569bdc046956690
Millisa G <135849424+millisag@users.noreply.github.com>
2025-01-27 12:59:23 -0500

    Third JavaScript class

---
+++ 03.js/03.js
@@ -0,0 +1,39 @@
+// Exercise 1 
+
+ const car = {
+    make: "Toyota",
+    model: "Prius",
+    year: 2010,
+    getInfo: function() {
+      console.log(`Car: ${this.year} ${this.make} ${this.model}`);
+    }
+  };
+  
+  car.getInfo(); 
+
+// Exercise 2
+
+/* function averageGrade(grades) {
+  let total = 0;
+  let amountOfGrades = grades.length;
+  console.log(total);
+  console.log(amountOfGrades);
+
+  for (let index = 0; index < grades.length; index++) {
+    let currentIndex = grades[index];
+    console.log(currentIndex);
+    total += currentIndex;
+  }
+
+  console.log(Math.round(total / amountOfGrades));
+}
+
+averageGrade([90, 80, 100]);*/ 
+
+
+
+
+
+
+
+
